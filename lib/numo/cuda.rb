cf1176871bc648917e2ddf29b6c42d9cac18efb8
sonots <sonots@gmail.com>
2017-12-06 16:01:05 +0900

    CUDA::Device

--- lib/numo/cuda.rb
+++ lib/numo/cuda.rb
@@ -3,8 +3,9 @@ module Numo
   end
 end
 
-require_relative 'cuda/compiler'
 require_relative 'cuda/compile_error'
-require_relative 'cuda/nvrtc_program'
+require_relative 'cuda/compiler'
+require_relative 'cuda/device'
 require_relative 'cuda/module'
 require_relative 'cuda/link_state'
+require_relative 'cuda/nvrtc_program'
---
+++ test/cuda/device_test.rb
@@ -0,0 +1,28 @@
+require_relative "../test_helper"
+
+module Numo::CUDA
+  class DeviceTest < Test::Unit::TestCase
+    def test_initialize
+      assert { Device.new(0).id == 0 }
+      assert { Device.new.id.is_a?(Integer) }
+    end
+
+    def test_use
+      assert_nothing_raised { Device.new(0).use }
+    end
+
+    def test_with
+      Device.new(0).with do
+        assert { Device.new.id == 0 }
+      end
+    end
+
+    def test_synchronize
+      assert_nothing_raised { Device.new.synchronize }
+    end
+    
+    def test_compute_capability
+      assert { Device.new.compute_capability.size == 2}
+    end
+  end
+end
