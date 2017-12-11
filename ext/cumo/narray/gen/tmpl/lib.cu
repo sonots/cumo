bc1baa85fdab86a5e5688c420fdf7f89a62d4c8e
sonots <sonots@gmail.com>
2017-12-11 21:31:41 +0900

    extern "C"

--- ext/cumo/narray/gen/tmpl/lib.cu
+++ ext/cumo/narray/gen/tmpl/lib.cu
@@ -1,6 +1,20 @@
 #include "cumo/narray_kernel.h"
 #include <<%="cumo/types/#{type_name}_kernel.h"%>>
 
+#if defined(__cplusplus)
+extern "C" {
+#if 0
+} /* satisfy cc-mode */
+#endif
+#endif
+
 <% children.each do |c|%>
 <%= "#{c.result}\n\n" %>
 <% end %>
+
+#if defined(__cplusplus)
+#if 0
+{ /* satisfy cc-mode */
+#endif
+}  /* extern "C" { */
+#endif
