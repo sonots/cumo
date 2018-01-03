86b0db0df1a658f39bd985f9411805c29cbf21cf
sonots <sonots@gmail.com>
2018-01-03 17:02:38 +0900

    binary2 kernel (divmod)

--- ext/cumo/narray/gen/tmpl/binary2.c
+++ ext/cumo/narray/gen/tmpl/binary2.c
@@ -1,3 +1,8 @@
+<% if c_iter.include?('robject') %>
+<% else %>
+void <%="#{c_iter}_stride_kernel_launch"%>(char *p1, char *p2, char *p3, char *p4, ssize_t s1, ssize_t s2, ssize_t s3, ssize_t s4, size_t n);
+<% end %>
+
 static void
 <%=c_iter%>(na_loop_t *const lp)
 {
@@ -11,6 +16,7 @@ static void
     INIT_PTR(lp, 2, p3, s3);
     INIT_PTR(lp, 3, p4, s4);
     for (i=n; i--;) {
+        <% if c_iter.include?('robject') %>
         GET_DATA_STRIDE(p1,s1,dtype,x);
         GET_DATA_STRIDE(p2,s2,dtype,y);
 <% if is_int and %w[divmod].include? name %>
@@ -22,6 +28,9 @@ static void
         m_<%=name%>(x,y,a,b);
         SET_DATA_STRIDE(p3,s3,dtype,a);
         SET_DATA_STRIDE(p4,s4,dtype,b);
+        <% else %>
+        <%="#{c_iter}_stride_kernel_launch"%>(p1,p2,p3,p4,s1,s2,s3,s4,n);
+        <% end %>
     }
 }
 
