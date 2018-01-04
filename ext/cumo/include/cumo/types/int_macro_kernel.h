7e5d0e45a403011f83bbd9630589da15bd5632bf
sonots <sonots@gmail.com>
2018-01-05 00:11:26 +0900

    define m_sign

--- ext/cumo/include/cumo/types/int_macro_kernel.h
+++ ext/cumo/include/cumo/types/int_macro_kernel.h
@@ -1,2 +1,3 @@
 #include "xint_macro_kernel.h"
 
+#define m_sign(x)    (((x)==0) ? 0 : (((x)>0) ? 1 : -1))
--- ext/cumo/include/cumo/types/uint_macro_kernel.h
+++ ext/cumo/include/cumo/types/uint_macro_kernel.h
@@ -1,2 +1,4 @@
 #include "xint_macro_kernel.h"
 
+#define m_abs(x)     (x)
+#define m_sign(x)    (((x)==0) ? 0:1)
