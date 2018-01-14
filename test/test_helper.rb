b65fd2ad2a036d5421669526abd7293692a6ead4
sonots <sonots@gmail.com>
2018-01-14 22:08:29 +0900

    cast to integer

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,6 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "cumo"
-Cumo::CUDA::Runtime.cudaSetDevice(ENV['CUDA_DEVICE'] || 0)
+Cumo::CUDA::Runtime.cudaSetDevice(Integer(ENV['CUDA_DEVICE'] || 0))
 
 require "pry"
 require "test/unit"
