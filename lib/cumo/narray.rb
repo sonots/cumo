f427ecf910cec52cb511efadbe8fa14caaf1fbf9
sonots <sonots@gmail.com>
2018-01-19 23:49:30 +0900

    Fix lib/cumo/narray.rb

--- lib/cumo/narray.rb
+++ lib/cumo/narray.rb
@@ -1,8 +1,2 @@
-begin
-  major, minor, _ = RUBY_VERSION.split(/\./)
-  require "#{major}.#{minor}/cumo/narray.so"
-rescue LoadError
-  require "cumo/narray.so"
-end
-
-require "cumo/narray/extra"
+# This file is for compatibility with require 'numo/narray'
+require_relative '../cumo'
