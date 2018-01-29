73eb60268b2edc52d95ad65133135eb32f395319
Naotoshi Seo <sonots@gmail.com>
2018-01-30 00:42:15 +0900

    aref.c: cudaDeviceSynchronize() to m_extract

--- ext/cumo/narray/gen/tmpl/aref.c
+++ ext/cumo/narray/gen/tmpl/aref.c
@@ -47,7 +47,10 @@ static VALUE
     if (nd) {
         return na_aref_main(argc, argv, self, 0, nd);
     } else {
+        // TODO(sonots): Return 0-dimensional narray rather than Synchronize()
         ptr = na_get_pointer_for_read(self) + pos;
+        SHOW_SYNCHRONIZE_WARNING_ONCE("<%=name%>", "<%=type_name%>");
+        cumo_cuda_runtime_check_status(cudaDeviceSynchronize());
         return m_extract(ptr);
     }
 }
