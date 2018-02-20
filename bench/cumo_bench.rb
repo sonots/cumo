c9fa8a7ff2ca914e4c625eccc8a7f6009d52340b
Naotoshi Seo <sonots@gmail.com>
2018-02-21 03:57:55 +0900

    Benchmark of small and large sized array for simple element-wise function

--- bench/cumo_bench.rb
+++ bench/cumo_bench.rb
@@ -1,15 +1,27 @@
 require 'cumo/narray'
 require 'benchmark'
 
-a = Cumo::Int32.new(10).seq(1)
-b = Cumo::Int32.new(10).seq(10,10)
-c = a + b
+# warm up
+a = Cumo::Int32.ones(10)
+a * 2
 
-a = Cumo::Int32.new(10000).seq(1)
-b = Cumo::Int32.new(10000).seq(10,10)
-Benchmark.bm do |r|
-  r.report do
-    100000.times { c = a + b}
-  end
+# Size
+# 104 105 106 107 108
+# NumPy [ms] 0.03 0.20 2.00 55.55 517.17
+# CuPy [ms] 0.58 0.97 1.84 12.48 84.73
+
+[4, 5, 6, 7, 8].each do |digit|
+  size = 10 ** digit
+  Cumo::CUDA::Runtime.cudaDeviceSynchronize
+  started = Time.now
+  a = Cumo::Int32.ones(size)
+  a * 2
+  Cumo::CUDA::Runtime.cudaDeviceSynchronize
+  puts "10**#{digit}: #{(Time.now - started).to_f * 1000} msec"
 end
 
+# 10**4: 0.43667100000000003 msec
+# 10**5: 0.35122000000000003 msec
+# 10**6: 1.0603609999999999 msec
+# 10**7: 7.784008999999999 msec
+# 10**8: 74.664282 msec
--- bench/numo_bench.rb
+++ bench/numo_bench.rb
@@ -1,14 +1,20 @@
 require 'numo/narray'
 require 'benchmark'
 
-a = Numo::Int32.new(10).seq(1)
-b = Numo::Int32.new(10).seq(10,10)
-c = a + b
+# warm up
+a = Numo::Int32.ones(10)
+a * 2
 
-a = Numo::Int32.new(10000).seq(1)
-b = Numo::Int32.new(10000).seq(10,10)
-Benchmark.bm do |r|
-  r.report do
-    100000.times { c = a + b}
-  end
+[4, 5, 6, 7, 8].each do |digit|
+  size = 10 ** digit
+  started = Time.now
+  a = Numo::Int32.ones(size)
+  a * 2
+  puts "10**#{digit}: #{(Time.now - started).to_f * 1000} msec"
 end
+
+# 10**4: 0.114593 msec
+# 10**5: 0.919754 msec
+# 10**6: 8.196666 msec
+# 10**7: 80.11726800000001 msec
+# 10**8: 721.842587 msec
