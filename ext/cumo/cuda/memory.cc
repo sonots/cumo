488291f15c132f95a3a3397c64423ace1701dda0
sonots <sonots@gmail.com>
2018-03-21 17:04:35 +0900

    Provide CUMO_MEMORY_POOL=ON environment variable to turn on memory pool

--- ext/cumo/cuda/memory.cc
+++ ext/cumo/cuda/memory.cc
@@ -4,15 +4,33 @@
 #include "cumo/cuda/memory_pool.h"
 #include "cumo/cuda/runtime.h"
 
+#include <cstdlib>
+#include <string>
+
 cumo::internal::MemoryPool pool{};
+
+// default: false (yet)
+bool
+cumo_cuda_memory_pool_p()
+{
+    const static char* env = std::getenv("CUMO_MEMORY_POOL");
+    static bool enabled = (env != nullptr && std::string(env) != "OFF");
+    return enabled;
+}
 
 char*
 cumo_cuda_runtime_malloc(size_t size)
 {
-    try {
-        return reinterpret_cast<char*>(pool.Malloc(size));
-    } catch (const cumo::internal::CUDARuntimeError& e) {
-        cumo_cuda_runtime_check_status(e.status());
+    if (cumo_cuda_memory_pool_p()) {
+        try {
+            return reinterpret_cast<char*>(pool.Malloc(size));
+        } catch (const cumo::internal::CUDARuntimeError& e) {
+            cumo_cuda_runtime_check_status(e.status());
+        }
+    } else {
+        void *ptr = 0;
+        cumo_cuda_runtime_check_status(cudaMallocManaged(&ptr, size, cudaMemAttachGlobal));
+        return reinterpret_cast<char*>(ptr);
     }
     return 0; // should not reach here
 }
@@ -20,9 +38,13 @@ cumo_cuda_runtime_malloc(size_t size)
 void
 cumo_cuda_runtime_free(char *ptr)
 {
-    try {
-        pool.Free(reinterpret_cast<intptr_t>(ptr));
-    } catch (const cumo::internal::CUDARuntimeError& e) {
-        cumo_cuda_runtime_check_status(e.status());
+    if (cumo_cuda_memory_pool_p()) {
+        try {
+            pool.Free(reinterpret_cast<intptr_t>(ptr));
+        } catch (const cumo::internal::CUDARuntimeError& e) {
+            cumo_cuda_runtime_check_status(e.status());
+        }
+    } else {
+        cumo_cuda_runtime_check_status(cudaFree((void*)ptr));
     }
 }
