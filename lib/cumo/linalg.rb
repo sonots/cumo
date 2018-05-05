8fab10a6618b2e26dbf487df438bfc48dce9c4c0
sonots <sonots@gmail.com>
2018-05-06 06:17:23 +0900

    Provide compatibility layer with numo/linalg (Cumo::Blas module)

---
+++ lib/cumo/linalg.rb
@@ -0,0 +1,12 @@
+require 'cumo'
+
+# Provide compatibility layers with numo/linalg
+module Cumo
+  module Blas
+    class << self
+      def gemm(a, *args, **kwargs)
+        a.gemm(*args, **kwargs)
+      end
+    end
+  end
+end
