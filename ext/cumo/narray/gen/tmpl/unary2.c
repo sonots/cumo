b9c4e6be8d8dff404c9ceafdc9411ac5156096bf
sonots <sonots@gmail.com>
2018-05-23 20:06:40 +0900

    add missing cudaDeviceSynchronize

--- ext/cumo/narray/gen/tmpl/unary2.c
+++ ext/cumo/narray/gen/tmpl/unary2.c
@@ -11,6 +11,7 @@ static void
     INIT_PTR_IDX(lp, 0, p1, s1, idx1);
     INIT_PTR_IDX(lp, 1, p2, s2, idx2);
     SHOW_SYNCHRONIZE_FIXME_WARNING_ONCE("<%=name%>", "<%=type_name%>");
+    cumo_cuda_runtime_check_status(cudaDeviceSynchronize());
     if (idx1) {
         if (idx2) {
             for (; i--;) {
