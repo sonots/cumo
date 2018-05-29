01e545f0096b066200c52a26290b0715bd14bd50
sonots <sonots@gmail.com>
2018-05-29 16:53:06 +0900

    Update benchmark of element-wise

--- bench/cumo_bench.rb
+++ bench/cumo_bench.rb
@@ -1,23 +1,57 @@
 require 'cumo/narray'
 require 'benchmark'
 
-NUM = (ARGV.first || 100000).to_i
+NUM = (ARGV.first || 100).to_i
 
 a = Cumo::Int32.new(10).seq(1)
 b = Cumo::Int32.new(10).seq(10,10)
 c = a + b
 c.free
 
-a = Cumo::Int32.new(10000).seq(1)
-b = Cumo::Int32.new(10000).seq(10,10)
+puts 'element-wise'
 Benchmark.bm do |r|
-  r.report(NUM) do
+  a = Cumo::Int32.new(10000).seq(1)
+  b = Cumo::Int32.new(10000).seq(10,10)
+  r.report('10**4') do
     NUM.times { (a + b).free }
+    Cumo::CUDA::Runtime.cudaDeviceSynchronize
+  end
+
+  a = Cumo::Int32.new(100000).seq(1)
+  b = Cumo::Int32.new(100000).seq(10,10)
+  r.report('10**5') do
+    NUM.times { (a + b).free }
+    Cumo::CUDA::Runtime.cudaDeviceSynchronize
+  end
+
+  a = Cumo::Int32.new(1000000).seq(1)
+  b = Cumo::Int32.new(1000000).seq(10,10)
+  r.report('10**6') do
+    NUM.times { (a + b).free }
+    Cumo::CUDA::Runtime.cudaDeviceSynchronize
+  end
+
+  a = Cumo::Int32.new(10000000).seq(1)
+  b = Cumo::Int32.new(10000000).seq(10,10)
+  r.report('10**7') do
+    NUM.times { (a + b).free }
+    Cumo::CUDA::Runtime.cudaDeviceSynchronize
+  end
+
+  a = Cumo::Int32.new(100000000).seq(1)
+  b = Cumo::Int32.new(100000000).seq(10,10)
+  r.report('10**8') do
+    NUM.times { (a + b).free }
+    Cumo::CUDA::Runtime.cudaDeviceSynchronize
   end
 end
 
-#              user     system      total        real
-#    10000  0.100000   0.000000   0.100000 (  0.099510)
-#   100000  0.880000   0.000000   0.880000 (  0.877870)
-#  1000000  7.180000   0.000000   7.180000 (  7.178121)
-# 10000000 83.140000   0.040000  83.180000 ( 83.179552)
+# Tesla V100-SXM2...
+#
+# element-wise
+#        user     system      total        real
+# 10**4  0.000000   0.000000   0.000000 (  0.000805)
+# 10**5  0.000000   0.000000   0.000000 (  0.000959)
+# 10**6  0.010000   0.000000   0.010000 (  0.004781)
+# 10**7  0.030000   0.010000   0.040000 (  0.045121)
+# 10**8  0.290000   0.160000   0.450000 (  0.443958
--- bench/numo_bench.rb
+++ bench/numo_bench.rb
@@ -1,7 +1,7 @@
 require 'numo/narray'
 require 'benchmark'
 
-NUM = (ARGV.first || 100000).to_i
+NUM = (ARGV.first || 100).to_i
 
 # warm up
 a = Numo::Int32.new(10).seq(1)
@@ -11,13 +11,43 @@ c = a + b
 a = Numo::Int32.new(10000).seq(1)
 b = Numo::Int32.new(10000).seq(10,10)
 Benchmark.bm do |r|
-  r.report(NUM) do
-    NUM.times { a + b }
+  a = Numo::Int32.new(10000).seq(1)
+  b = Numo::Int32.new(10000).seq(10,10)
+  r.report('10**4') do
+    NUM.times { (a + b) }
+  end
+
+  a = Numo::Int32.new(100000).seq(1)
+  b = Numo::Int32.new(100000).seq(10,10)
+  r.report('10**5') do
+    NUM.times { (a + b) }
+  end
+
+  a = Numo::Int32.new(1000000).seq(1)
+  b = Numo::Int32.new(1000000).seq(10,10)
+  r.report('10**6') do
+    NUM.times { (a + b) }
+  end
+
+  a = Numo::Int32.new(10000000).seq(1)
+  b = Numo::Int32.new(10000000).seq(10,10)
+  r.report('10**7') do
+    NUM.times { (a + b) }
+  end
+
+  a = Numo::Int32.new(100000000).seq(1)
+  b = Numo::Int32.new(100000000).seq(10,10)
+  r.report('10**8') do
+    NUM.times { (a + b) }
   end
 end
 
-# 10**4: 0.114593 msec
-# 10**5: 0.919754 msec
-# 10**6: 8.196666 msec
-# 10**7: 80.11726800000001 msec
-# 10**8: 721.842587 msec
+# Intel(R) Xeon(R) CPU E5-2686 v4 @ 2.30GHz
+#
+# element-wise
+#        user     system      total        real
+# 10**4  0.000000   0.000000   0.000000 (  0.002165)
+# 10**5  0.000000   0.020000   0.020000 (  0.021530)
+# 10**6  0.080000   0.040000   0.120000 (  0.117102)
+# 10**7  0.980000   0.900000   1.880000 (  1.880362)
+# 10**8  9.490000   8.580000  18.070000 ( 18.067418)
