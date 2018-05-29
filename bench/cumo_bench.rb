e85268cc13f738698715d1d0cb974e2784a2b5fc
sonots <sonots@gmail.com>
2018-05-29 17:24:34 +0900

    Add benchmark of dot

--- bench/cumo_bench.rb
+++ bench/cumo_bench.rb
@@ -103,8 +103,65 @@ def reduction
   end
 end
 
-elementwise
-reduction
+def dot
+  num = 3
+  puts 'dot'
+  Benchmark.bm do |r|
+    a = Cumo::Float32.new(100,100).seq(1)
+    b = Cumo::Float32.new(100,100).seq(10,10)
+    a.dot(b).free # warm up
+    r.report('10**4') do
+      num.times do
+        a.dot(b).free
+        Cumo::CUDA::Runtime.cudaDeviceSynchronize
+      end
+    end
+
+    a = Cumo::Float32.new(100,1000).seq(1)
+    b = Cumo::Float32.new(1000,100).seq(10,10)
+    a.dot(b).free # warm up
+    r.report('10**5') do
+      num.times do
+        a.dot(b).free
+        Cumo::CUDA::Runtime.cudaDeviceSynchronize
+      end
+    end
+
+    a = Cumo::Float32.new(100,10000).seq(1)
+    b = Cumo::Float32.new(10000,100).seq(10,10)
+    a.dot(b).free # warm up
+    r.report('10**6') do
+      num.times do
+        a.dot(b).free
+        Cumo::CUDA::Runtime.cudaDeviceSynchronize
+      end
+    end
+
+    a = Cumo::Float32.new(100,100000).seq(1)
+    b = Cumo::Float32.new(100000,100).seq(10,10)
+    a.dot(b).free # warm up
+    r.report('10**7') do
+      num.times do
+        a.dot(b).free
+        Cumo::CUDA::Runtime.cudaDeviceSynchronize
+      end
+    end
+
+    a = Cumo::Float32.new(100,1000000).seq(1)
+    b = Cumo::Float32.new(1000000,100).seq(10,10)
+    a.dot(b).free # warm up
+    r.report('10**8') do
+      num.times do
+        a.dot(b).free
+        Cumo::CUDA::Runtime.cudaDeviceSynchronize
+      end
+    end
+  end
+end
+
+# elementwise
+# reduction
+dot
 
 # Tesla V100-SXM2...
 #
@@ -122,3 +179,10 @@ reduction
 # 10**6  0.100000   0.050000   0.150000 (  0.152070)
 # 10**7  1.150000   0.600000   1.750000 (  1.754977)
 # 10**8 11.720000   5.750000  17.470000 ( 17.470990)
+# dot
+#        user     system      total        real
+# 10**4  0.000000   0.000000   0.000000 (  0.000351)
+# 10**5  0.000000   0.000000   0.000000 (  0.000838)
+# 10**6  0.000000   0.000000   0.000000 (  0.002702)
+# 10**7  0.020000   0.010000   0.030000 (  0.024650)
+# 10**8  0.180000   0.060000   0.240000 (  0.245101)
--- bench/numo_bench.rb
+++ bench/numo_bench.rb
@@ -73,8 +73,45 @@ def reduction
   end
 end
 
-elementwise
-reduction
+def dot
+  num = 3
+  puts 'dot'
+  Benchmark.bm do |r|
+    a = Numo::Float32.new(100,100).seq(1)
+    b = Numo::Float32.new(100,100).seq(10,10)
+    r.report('10**4') do
+      num.times { a.dot(b) }
+    end
+
+    a = Numo::Float32.new(100,1000).seq(1)
+    b = Numo::Float32.new(1000,100).seq(10,10)
+    r.report('10**5') do
+      num.times { a.dot(b) }
+    end
+
+    a = Numo::Float32.new(100,10000).seq(1)
+    b = Numo::Float32.new(10000,100).seq(10,10)
+    r.report('10**6') do
+      num.times { a.dot(b) }
+    end
+
+    a = Numo::Float32.new(100,100000).seq(1)
+    b = Numo::Float32.new(100000,100).seq(10,10)
+    r.report('10**7') do
+      num.times { a.dot(b) }
+    end
+
+    a = Numo::Float32.new(100,1000000).seq(1)
+    b = Numo::Float32.new(1000000,100).seq(10,10)
+    r.report('10**8') do
+      num.times { a.dot(b) }
+    end
+  end
+end
+
+# elementwise
+# reduction
+dot
 
 # Intel(R) Xeon(R) CPU E5-2686 v4 @ 2.30GHz
 #
@@ -92,3 +129,10 @@ reduction
 # 10**6  0.060000   0.000000   0.060000 (  0.063871)
 # 10**7  0.640000   0.000000   0.640000 (  0.641754)
 # 10**8  7.070000   0.000000   7.070000 (  7.071800)
+# dot
+#        user     system      total        real
+# 10**4  0.000000   0.000000   0.000000 (  0.003943)
+# 10**5  0.040000   0.000000   0.040000 (  0.037703)
+# 10**6  0.390000   0.000000   0.390000 (  0.383590)
+# 10**7  3.800000   0.000000   3.800000 (  3.798749)
+# 10**8 38.790000   0.000000  38.790000 ( 38.787569)
