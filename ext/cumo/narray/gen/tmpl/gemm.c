4d6e498eb75e24b6e823358af3ed7c6b165a5ea7
sonots <sonots@gmail.com>
2018-06-07 19:46:42 +0900

    No ndloop version

--- ext/cumo/narray/gen/tmpl/gemm.c
+++ ext/cumo/narray/gen/tmpl/gemm.c
@@ -23,17 +23,10 @@
     end
 %>
 
-#define args_t <%=name%>_args_t
-
 typedef struct {
-    // enum CBLAS_ORDER order; // cuBLAS does not have order (row-major or column-major) option
-    // cublasOperation_t transa, transb;
-    // cublasSideMode_t side;
-    // cublasFillMode_t uplo;
-    // cublasDiagType_t diag;
     dtype alpha, beta;
     int m, n, k;
-} args_t;
+} gemm_args_t;
 
 typedef struct {
     int ld;
@@ -41,53 +34,53 @@ typedef struct {
 } gemm_layout_t;
 
 static bool
-is_f_contiguous(na_loop_args_t* arg) {
-    size_t step = arg->elmsz;
-    if (arg->ndim == 0) {
-        return true;
-    }
-    if (arg->ndim == 1 && arg->shape[0] == 1) {
-        return true;
-    }
-    for (int i = 0; i < arg->ndim; ++i) {
-        if (arg->shape[i] == 1) continue;
-        if (arg->iter[i].step != step) {
-            return false;
+is_f_contiguous(VALUE a)
+{
+    int i;
+    ssize_t s0;
+    narray_t *na;
+
+    switch(RNARRAY_TYPE(a)) {
+    case NARRAY_DATA_T:
+    case NARRAY_FILEMAP_T:
+        return TEST_COLUMN_MAJOR(a);
+    case NARRAY_VIEW_T:
+        GetNArray(a, na);
+
+        // not contiguous if it has index
+        for (i = 0; i < NA_NDIM(na); ++i) {
+            if (NA_IS_INDEX_AT(na, i)) return false;
         }
-        step *= arg->shape[i];
+
+        // check f-contiguous
+        s0 = nary_element_stride(a);
+        for (i = 0; i < NA_NDIM(na); ++i) {
+            if (NA_SHAPE(na)[i] == 1) continue;
+            if (NA_STRIDE_AT(na, i) != s0) return false;
+            s0 *= NA_SHAPE(na)[i];
+        }
+        return true;
+    default:
+        rb_raise(rb_eArgError, "NArray type : %d is not supported", RNARRAY_TYPE(a));
     }
-    return true;
 }
 
 static bool
-is_c_contiguous(na_loop_args_t* arg) {
-    size_t step = arg->elmsz;
-    if (arg->ndim == 0) {
-        return true;
-    }
-    if (arg->ndim == 1 && arg->shape[0] == 1) {
-        return true;
-    }
-    for (int i = arg->ndim - 1; i >= 0 ; --i) {
-        if (arg->shape[i] == 1) continue;
-        if (arg->iter[i].step != step) {
-            return false;
-        }
-        step *= arg->shape[i];
-    }
-    return true;
+is_c_contiguous(VALUE a)
+{
+    return na_check_contiguous(a) == Qtrue;
 }
 
 static gemm_layout_t
-make_gemm_layout(na_loop_args_t* arg)
+make_gemm_layout(VALUE a)
 {
-    assert(arg->ndim == 2);
+    assert(RNARRAY_NDIM(a) == 2);
     gemm_layout_t layout;
-    if (is_f_contiguous(arg)) {
-        layout.ld = arg->shape[0];
+    if (is_f_contiguous(a)) {
+        layout.ld = RNARRAY_SHAPE(a)[0];
         layout.trans = CUBLAS_OP_T;
-    } else if (is_c_contiguous(arg)) {
-        layout.ld = arg->shape[1];
+    } else if (is_c_contiguous(a)) {
+        layout.ld = RNARRAY_SHAPE(a)[1];
         layout.trans = CUBLAS_OP_N;  // transposed
     } else {
         // TODO(sonots): Make contiguous array and compute with it
@@ -99,7 +92,7 @@ make_gemm_layout(na_loop_args_t* arg)
 extern int na_debug_flag;  // narray.c
 
 static void
-print_gemm_args(args_t* g, gemm_layout_t* a_layout, gemm_layout_t* b_layout)
+print_gemm_args(gemm_args_t* g, gemm_layout_t* a_layout, gemm_layout_t* b_layout)
 {
     printf("transb=%d transa=%d, n=%d, m=%d, k=%d, ldb=%d, lda=%d, ldc=n=%d\n",
             (int)b_layout->trans,
@@ -113,24 +106,16 @@ print_gemm_args(args_t* g, gemm_layout_t* a_layout, gemm_layout_t* b_layout)
 }
 
 static void
-<%=c_iter%>(na_loop_t *const lp)
+<%=c_iter%>(VALUE a, VALUE b, VALUE c, gemm_args_t *g)
 {
-    dtype *a, *b;
     gemm_layout_t a_layout, b_layout;
-    dtype *c;
-    args_t *g;
     cublasHandle_t handle = 0;
     cublasStatus_t status = 0;
 
-    a = (dtype*)NDL_PTR(lp,0);
-    b = (dtype*)NDL_PTR(lp,1);
-    c = (dtype*)NDL_PTR(lp,2);
-    g = (args_t*)(lp->opt_ptr);
-
     // TODO(sonots): Use gemmStridedBatched to support ndim >= 2 in batch
 
-    a_layout = make_gemm_layout(&lp->args[0]);
-    b_layout = make_gemm_layout(&lp->args[1]);
+    a_layout = make_gemm_layout(a);
+    b_layout = make_gemm_layout(b);
 
     // Note that cuBLAS uses the column major matrix representation.
     // We use technic which following site describes:
@@ -156,12 +141,12 @@ static void
             g->m,
             g->k,
             (<%=cutype%>*)(&g->alpha),
-            (<%=cutype%>*)b,
+            (<%=cutype%>*)na_get_pointer_for_read(b),
             b_layout.ld,
-            (<%=cutype%>*)a,
+            (<%=cutype%>*)na_get_pointer_for_read(a),
             a_layout.ld,
             (<%=cutype%>*)(&g->beta),
-            (<%=cutype%>*)c,
+            (<%=cutype%>*)na_get_pointer_for_write(c),
             g->n);
     cublasDestroy(handle);
     cumo_cuda_cublas_check_status(status);
@@ -209,65 +194,53 @@ static VALUE
 <%=c_func(-1)%>(int argc, VALUE argv[], VALUE self)
 {
     VALUE     a=self, b, c=Qnil, alpha, beta;
-    narray_t *na1, *na2;
+    narray_t *na, *nb;
     size_t    out_shape[2];
-    ndfunc_arg_in_t ain[3] = {{cT,2},{cT,2},{OVERWRITE,2}};
-    ndfunc_arg_out_t aout[1] = {{cT,2,out_shape}};
-    ndfunc_t ndf = {<%=c_iter%>, NO_LOOP, 3, 0, ain, aout};
 
-    args_t g;
+    gemm_args_t g;
     VALUE kw_hash = Qnil;
-    ID kw_table[4] = {rb_intern("alpha"),rb_intern("beta")};
-    VALUE opts[4] = {Qundef,Qundef};
+    ID kw_table[2] = {rb_intern("alpha"), rb_intern("beta")};
+    VALUE opts[2] = {Qundef, Qundef};
 
     rb_scan_args(argc, argv, "11:", &b, &c, &kw_hash);
-    rb_get_kwargs(kw_hash, kw_table, 0, 4, opts);
-    alpha    = option_value(opts[0],Qnil);
-    g.alpha  = RTEST(alpha) ? m_num_to_data(alpha) : m_one;
-    beta     = option_value(opts[1],Qnil);
-    g.beta   = RTEST(beta)  ? m_num_to_data(beta)  : m_zero;
+    rb_get_kwargs(kw_hash, kw_table, 0, 2, opts);
+    alpha   = option_value(opts[0],Qnil);
+    g.alpha = RTEST(alpha) ? m_num_to_data(alpha) : m_one;
+    beta    = option_value(opts[1],Qnil);
+    g.beta  = RTEST(beta)  ? m_num_to_data(beta)  : m_zero;
 
-    GetNArray(a,na1);
-    GetNArray(b,na2);
-    CHECK_DIM_GE(na1,2);
-    CHECK_DIM_GE(na2,2);
+    GetNArray(a, na);
+    GetNArray(b, nb);
 
-    g.m = ROW_SIZE(na1);
-    g.k = COL_SIZE(na1);
-    g.n = COL_SIZE(na2);
+    // TODO(sonots): support ndim > 2
+    CHECK_DIM_EQ(na, 2);
+    CHECK_DIM_EQ(nb, 2);
 
-    if (ROW_SIZE(na2) != g.k) {
-        rb_raise(nary_eShapeError,"row size of b %d must equal to col size of a %d", ROW_SIZE(na2), g.k);
+    g.m = ROW_SIZE(na);
+    g.k = COL_SIZE(na);
+    g.n = COL_SIZE(nb);
+
+    if (ROW_SIZE(nb) != g.k) {
+        rb_raise(nary_eShapeError,"row size of b %d must equal to col size of a %d", (int)ROW_SIZE(nb), g.k);
     }
 
     if (c == Qnil) { // c is not given.
-        ndfunc_arg_in_t ain_init = {sym_init,0};
-        ain[2] = ain_init;
-        ndf.nout = 1;
-        c = INT2FIX(0);
         out_shape[0] = g.m;
         out_shape[1] = g.n;
+        c = nary_new(cT, 2, out_shape);
     } else {
-        narray_t *na3;
-        COPY_OR_CAST_TO(c,cT);
-        GetNArray(c,na3);
-        CHECK_DIM_GE(na3,2);
-        if (ROW_SIZE(na3) != g.m) {
-            rb_raise(nary_eShapeError,"row size of c %d must equal to row size of a %d", ROW_SIZE(na3), g.m);
+        narray_t *nc;
+        COPY_OR_CAST_TO(c, cT);
+        GetNArray(c, nc);
+        CHECK_DIM_EQ(nc, 2);
+        if ((int)ROW_SIZE(nc) != g.m) {
+            rb_raise(nary_eShapeError,"row size of c %d must equal to row size of a %d", (int)ROW_SIZE(nc), g.m);
         }
-        if (COL_SIZE(na3) != g.n) {
-            rb_raise(nary_eShapeError,"col size of c %d must equal to col size of b %d", COL_SIZE(na3), g.n);
+        if ((int)COL_SIZE(nc) != g.n) {
+            rb_raise(nary_eShapeError,"col size of c %d must equal to col size of b %d", (int)COL_SIZE(nc), g.n);
         }
     }
-    {
-        VALUE ans = na_ndloop3(&ndf, &g, 3, a, b, c);
 
-        if (ndf.nout == 1) { // c is not given.
-            return ans;
-        } else {
-            return c;
-        }
-    }
+    <%=c_iter%>(a, b, c, &g);
+    return c;
 }
-
-#undef args_t
