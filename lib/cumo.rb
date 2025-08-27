5b3369925ca734ae72d8baa2e8ef2fb4cf58a7f2
Shizuo Fujita <fujita@clear-code.com>
2025-08-27 09:22:15 +0900

    Fix LoadError
    After installed cumo, `cumo.so` will be copied into `lib` directory automatically

--- lib/cumo.rb
+++ lib/cumo.rb
@@ -1,3 +1,7 @@
-require_relative File.join(__dir__, '../ext/cumo/cumo')
+begin
+  require_relative 'cumo.so'
+rescue LoadError
+  require_relative File.join(__dir__, '../ext/cumo/cumo')
+end
 require_relative 'cumo/cuda'
 require_relative 'cumo/narray/extra'
