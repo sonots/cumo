2d9c4badf36a5f939df610ac38418012dc4c66de
Shizuo Fujita <fujita@clear-code.com>
2025-12-28 16:00:46 +0900

    Add --gpus option to use host GPU

--- docker-launch.sh
+++ docker-launch.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 project_dir="$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)/"
-docker run -v $project_dir:/workspace -it cumo-dev bash
+docker run --gpus all -v $project_dir:/workspace -it cumo-dev bash
