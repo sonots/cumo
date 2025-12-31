7ee7349718d766dc81e1a2487d30ba19bf9ed5b8
Shizuo Fujita <fujita@clear-code.com>
2026-01-01 04:39:17 +0900

    Fix SEGV in Cumo::{DComplex, SComplex}#var with single element array

--- test/narray_test.rb
+++ test/narray_test.rb
@@ -751,6 +751,9 @@ class NArrayTest < Test::Unit::TestCase
 
     assert { Cumo::SFloat[1].var.to_f.nan? }
     assert { Cumo::DFloat[1].var.to_f.nan? }
+    assert { Cumo::SComplex[1].var.to_f.nan? }
+    assert { Cumo::DComplex[1].var.to_f.nan? }
+
     assert { Cumo::SFloat[1].stddev.to_f.nan? }
     assert { Cumo::DFloat[1].stddev.to_f.nan? }
     assert { Cumo::SFloat[1].rms == 1.0 }
